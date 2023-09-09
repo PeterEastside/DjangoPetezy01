ba029fbc6d3a1a336155e258ab2a98ec95ffb067
PeterEastside <125596681+PeterEastside@users.noreply.github.com>
2023-09-10 01:22:10 +0700

    Change from httpresponse to be render

--- Page1/views.py
+++ Page1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def HomePage(request):
-    return HttpResponse("<h1>HI GOD JADE<h1>")
+    return render(request, "Page1/homepage.html")
